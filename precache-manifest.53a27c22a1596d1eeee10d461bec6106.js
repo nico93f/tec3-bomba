8681675d335bfa10e0f59f7197a81b1ef35885a7
Nico <nico93f@gmail.com>
2019-10-31 22:13:03 -0300

    Updates

---
+++ precache-manifest.53a27c22a1596d1eeee10d461bec6106.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe48605d15241746426881421a66c35",
+    "url": "/tec3-bomba/index.html"
+  },
+  {
+    "revision": "ad8fcb5a000c0ddb185b",
+    "url": "/tec3-bomba/static/css/main.ae0cdfc8.chunk.css"
+  },
+  {
+    "revision": "361ae104a9e9086e9abd",
+    "url": "/tec3-bomba/static/js/2.7e2a3cf1.chunk.js"
+  },
+  {
+    "revision": "ad8fcb5a000c0ddb185b",
+    "url": "/tec3-bomba/static/js/main.4ae2b0ef.chunk.js"
+  },
+  {
+    "revision": "2231db9ade5a9eea8252",
+    "url": "/tec3-bomba/static/js/runtime-main.e6c0f830.js"
+  },
+  {
+    "revision": "5485284455b5a25265f5eb8990fd9d0e",
+    "url": "/tec3-bomba/static/media/background.54852844.jpg"
+  },
+  {
+    "revision": "b006268b9b53a8ada84c1caf0cf79a42",
+    "url": "/tec3-bomba/static/media/beep.b006268b.mp3"
+  },
+  {
+    "revision": "b729672e737748764a20ec9d092d0a62",
+    "url": "/tec3-bomba/static/media/correct.b729672e.mp3"
+  },
+  {
+    "revision": "c6b6aef25ebae6518af4be54ff524b83",
+    "url": "/tec3-bomba/static/media/danger.c6b6aef2.mp3"
+  },
+  {
+    "revision": "fac8e2e877452e7415a23ac7445273d8",
+    "url": "/tec3-bomba/static/media/warning.fac8e2e8.mp3"
+  }
+]);
